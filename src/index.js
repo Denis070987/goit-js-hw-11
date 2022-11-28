b616e4f6f5dd3827e95c73f2a3a8e7171f9e3e54
Denis070987 <deniskoms@gmail.com>
2022-11-28 12:02:27 +0200

    fix task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 const form = document.querySelector('#search-form');
-// search.addEventListener('input')
+const makeGallery = document.querySelector('.gallery') 
 const submitBtn = document.querySelector('button');
 
 form.addEventListener('submit', onSearch);
@@ -14,20 +14,50 @@ form.addEventListener('submit', onSearch);
  function onSearch(evt) {
      evt.preventDefault();
      const searchValue = form.searchQuery.value
-     axiosApi(searchValue).then(data => console.log(data)).catch(err => console.log(err))
+     axiosApi(searchValue).then(data => creatMarkup(data.hits)).catch(error => {if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+     }
+    console.log(error.config)})
 }
 
 async function axiosApi(name) {
-   
-     const response = await axios.get(`https://pixabay.com/api/?key=31616133-e43619194a5e3cbf561fcd52b&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`);
-    
-    const data = await response.data;
-    
+    const response = await axios.get(`https://pixabay.com/api/?key=31616133-e43619194a5e3cbf561fcd52b&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`);
+    const data =  response.data;
     return data
 }
 
-function creatMarkup() {
-   
+function creatMarkup(arr) {
+    const markup = arr.map(item => 
+        `<div class="photo-card">
+   <a href="${item.largeImageURL}"></a>
+  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span>${item.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <span>${item.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span>${item.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span>${item.downloads}</span>
+    </p>
+  </div>
+</div>`).join('');
+    
+    makeGallery.innerHTML = markup
 }
 
 
