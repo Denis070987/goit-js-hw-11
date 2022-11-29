efe486184a2b34b888f562ee3ac821a171869666
Denis070987 <deniskoms@gmail.com>
2022-11-29 16:05:53 +0200

    fix task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css'
 import axios from "axios";
-
+import Notiflix from 'notiflix';
 
 
 const form = document.querySelector('#search-form');
@@ -8,6 +8,7 @@ const makeGallery = document.querySelector('.gallery')
 const submitBtn = document.querySelector('button');
 const loadBtn = document.querySelector('.load-more')
 form.addEventListener('submit', onSearch);
+
 loadBtn.addEventListener('click', onLoad)
 
 loadBtn.setAttribute('hidden', true)
@@ -19,38 +20,39 @@ function onLoad(){
   page += 1
   axiosApi(searchValue, page).then(data => {
     makeGallery.insertAdjacentHTML('beforeend',creatMarkup(data.hits))
-    if (data.hits.length < 39 ) {
+    if (data.hits.length < 39) {
+      Notiflix.Notify.info('Were sorry, but youve reached the end of search results.')
       loadBtn.setAttribute('hidden', true)
-    }
+    } 
   })
 };
 
- function onSearch(evt) {
-     evt.preventDefault();
-   searchValue = form.searchQuery.value
-   if (searchValue === '') {
-     makeGallery.innerHTML = ''
-     console.log('not')
-     loadBtn.setAttribute('hidden', true)
-   } else {
-     loadBtn.removeAttribute('hidden', true)
-     axiosApi(searchValue).then(data => makeGallery.innerHTML = creatMarkup(data.hits)).catch(error => console.log(error.config))
-   }
+function onSearch(evt) {
+  evt.preventDefault();
+  searchValue = form.searchQuery.value
   
+  axiosApi(searchValue).then(data => {
+    if (!data.totalHits || !searchValue) {
+      loadBtn.setAttribute('hidden', true)
+       Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.')
+    } else { makeGallery.innerHTML = creatMarkup(data.hits) }
+  }).catch(error => console.log(error.config))
+    
 };
 
 async function axiosApi(name, page = 1) {
     const response = await axios.get(`https://pixabay.com/api/?key=31616133-e43619194a5e3cbf561fcd52b&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-    const data =  response.data;
-    return data
-};
+  const data = response.data;
+  return data
+  };
 
 
 
 function creatMarkup(arr) {
-  
-  return arr.map(item =>
-    `<div class="photo-card">
+   
+      loadBtn.removeAttribute('hidden', true)
+      return arr.map(item =>
+        `<div class="photo-card">
    <a href="${item.largeImageURL}"></a>
    <div class="thumb">
   <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
@@ -75,6 +77,9 @@ function creatMarkup(arr) {
     </div>
 </div>`).join('');
     
+
+      
+       
   // makeGallery.insertAdjacentHTML('beforeend', markup);
   // makeGallery.innerHTML = markup
   
